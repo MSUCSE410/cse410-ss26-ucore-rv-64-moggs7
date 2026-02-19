d48b19c2c660d4c718421e3e8cfa66114bc2d8d5
Maggie <136645748+moggs7@users.noreply.github.com>
2026-02-19 18:57:48 +0000

    Chapter 4 Submission

--- os/syscall.c
+++ os/syscall.c
@@ -6,6 +6,8 @@
 #include "trap.h"
 #include "proc.h"	// need to define TaskInfo
 #include "vm.h"
+#include "riscv.h"
+#include "kalloc.h"
 
 uint64 sys_write(int fd, uint64 va, uint len)
 {
@@ -57,6 +59,99 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz) // TODO: implement sys_gettimeofd
 // TODO: add support for mmap and munmap syscall.
 // hint: read through docstrings in vm.c. Watching CH4 video may also help.
 // Note the return value and PTE flags (especially U,X,W,R)
+
+int sys_mmap(void *start, uint64 len, int port, int flag, int fd)
+{
+	// quit if len is 0
+	if (len == 0) return 0;
+
+	// upper limit 1 GiB
+	if (len > (1ULL << 30)) return -1;
+
+	uint64 va0 = (uint64)start;
+
+    // start must be page aligned
+    if (!PGALIGNED(va0)) return -1;
+
+    // port must only use bits [0..2]
+    if ((port & ~0x7) != 0) return -1;
+
+    // port must not be 0 (meaningless mapping)
+    if ((port & 0x7) == 0) return -1;
+
+    struct proc *p = curr_proc();
+
+    // round length up to whole pages
+    uint64 sz = PGROUNDUP(len);
+
+    // translate port to PTE flags
+    int perm = PTE_U;
+    if (port & 0x1) perm |= PTE_R;
+    if (port & 0x2) perm |= PTE_W;
+    if (port & 0x4) perm |= PTE_X;
+
+    // error if any page alrea.dy mapped in [va0, va0+sz)
+    for (uint64 va = va0; va < va0 + sz; va += PGSIZE) 
+	{
+        if (walkaddr(p->pagetable, va) != 0) 
+		{
+            return -1;
+        }
+    }
+
+	// allocate + map each page
+	for (uint64 va = va0; va < va0 + sz; va += PGSIZE)
+	{
+		void *pa = kalloc();
+		if (pa == 0)
+		{
+			// don't reclaim on failure
+			return -1;
+		}
+		memset(pa, 0, PGSIZE);
+		if (mappages(p->pagetable, va, PGSIZE, (uint64)pa, perm) != 0)
+		{
+			// don't reclaim on failure
+			return -1;
+		}
+	}
+	return 0;
+}
+
+int sys_munmap(void *start, uint64 len)
+{
+	// quit if len is 0
+	if (len == 0) return 0;
+
+	// upper limit is 1 GiB
+	if (len > (1ULL << 30)) return -1;
+
+	uint64 va0 = (uint64)start;
+
+	// page align start
+	if (!PGALIGNED(va0)) return -1;
+
+	struct proc *p = curr_proc();
+
+	// round page length up to whole pages
+	uint64 sz = PGROUNDUP(len);
+
+	// if any page is unmapped in [va0, va0 + sz), error
+	for (uint64 va = va0; va < va0 + sz; va += PGSIZE)
+	{
+		if (walkaddr(p->pagetable, va) == 0)
+		{
+			return -1;
+		}
+	}
+
+	// pass number of pages
+	uvmunmap(p->pagetable, va0, sz / PGSIZE, 1);
+
+	return 0;
+}
+
+
 /*
 * LAB1: you may need to define sys_task_info here
 */
@@ -117,6 +212,13 @@ void syscall()
 	case SYS_gettimeofday:
 		ret = sys_gettimeofday((TimeVal *)args[0], args[1]);
 		break;
+	case SYS_mmap:
+		ret = sys_mmap((void*)args[0], args[1], args[2], args[3], args[4]);
+		break;
+	case SYS_munmap:
+		ret = sys_munmap((void*)args[0], args[1]);
+		break;
+
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
--- os/syscall.h
+++ os/syscall.h
@@ -1,6 +1,14 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#include "types.h"
+
 void syscall();
+
+//
+// Project 2 additions
+//
+int sys_mmap(void *start, uint64 len, int port, int flag, int fd);   ///< memory map
+int sys_munmap(void *start, uint64 len);                             ///< memory unmap
 
 #endif // SYSCALL_H
